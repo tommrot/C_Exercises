d7f92d8a6068dea86249d58b7f548ce4a48280d6
tommrot <tommasorotelli12@gmail.com>
2026-05-26 12:17:24 +0200

    bug fixed and added void* instead of int

--- linked_lists/insert_node.c
+++ linked_lists/insert_node.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void list_insert_in_head (Node **head, int value){
+void list_insert_in_head (Node **head, void *value){
     Node *new = malloc(sizeof(Node));
-    if (new = NULL){
+    if (new == NULL){
         printf("Allocation Error!\n");
         exit(0);
     }
@@ -15,7 +15,7 @@ void list_insert_in_head (Node **head, int value){
 
 
 
-void list_insert_after (Node *previous, int value){
+void list_insert_after (Node *previous, void *value){
     Node *new = malloc(sizeof(Node));
     if (new == NULL){
         printf("Allocation Error!\n");
@@ -28,20 +28,20 @@ void list_insert_after (Node *previous, int value){
 
 
 
-void list_append(Node **head, int value){
-    Node *new = head;
-    if (new = NULL){
+void list_append(Node **head, void *value){
+    Node *new = malloc(sizeof(Node));
+    if (new == NULL){
         printf("Allocation Error!\n");
         exit(0);
     } 
     new->value = value;
     new->next = NULL;
 
-    if (*head = NULL){
+    if (*head == NULL){
         *head = new;
         return;
     }
-    Node *current = head;
+    Node *current = *head;
     while (current->next != NULL){
         current = current->next;
     }
