9680b3f6f714e1cb200a456b405a77a32d394805
tommrot <tommasorotelli12@gmail.com>
2026-05-26 11:44:24 +0200

    added list_insert_after()

--- linked_lists/insert_node.c
+++ linked_lists/insert_node.c
@@ -1,5 +1,6 @@
 #include "node.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void list_insert_in_head (Node **head, int value){
     Node *new = malloc(sizeof(Node));
@@ -14,3 +15,13 @@ void list_insert_in_head (Node **head, int value){
 
 
 
+void list_insert_after (Node *previous, int value){
+    Node *new = malloc(sizeof(Node));
+    if (new == NULL){
+        printf("Allocation Error\n");
+        exit(0);
+    }
+    new->value = value;
+    new->next = previous->next;
+    previous->next = new;
+}
